51e93aed4636bb17eedb663cb79a628ea0a555d8
jlelito <joshualelito@gmail.com>
2020-11-29 11:10:32 -0500

    Forgot to take out commented function

--- client/src/App.js
+++ client/src/App.js
@@ -55,19 +55,6 @@ class App extends Component {
       
   }
   
-  //Loads Web3 to detect MetaMask
-  // async loadWeb3() {
-  //     if (window.ethereum) {
-  //       window.web3 = new Web3(window.ethereum)
-  //       await window.ethereum.enable()
-  //     }
-  //     else if (window.web3) {
-  //       window.web3 = new Web3(window.web3.currentProvider)
-  //     }
-  //     else {
-  //       window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
-  //     }
-  //   }
 
     
 
