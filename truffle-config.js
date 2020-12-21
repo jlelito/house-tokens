812a1b4d6de198a113deab6c234564f2c1bd6536
jlelito <joshualelito@gmail.com>
2020-12-20 19:57:36 -0500

    Moving contract to Ropsten testnet

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+require('dotenv').config();
+const HDWalletProvider = require('truffle-hdwallet-provider');
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -8,11 +10,25 @@ module.exports = {
     develop: {
       port: 7545
     },
+
     test: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*"
+    },
+
+    ropsten: {
+      provider: function(){
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`
+          )
+      },
+      gasPrice: 100000000000,
+      network_id: 3
     }
   }
+
+  
   
 }
