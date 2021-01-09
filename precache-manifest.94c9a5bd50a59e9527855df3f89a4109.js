d0900c457d30c949ba30f87e9448167d88173627
jlelito <joshualelito@gmail.com>
2021-01-09 18:31:46 -0500

    Updates

---
+++ precache-manifest.94c9a5bd50a59e9527855df3f89a4109.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "985be5158d573315a75ad69b1c0adc29",
+    "url": "/house-tokens/index.html"
+  },
+  {
+    "revision": "5222039d7fd86f0da4c9",
+    "url": "/house-tokens/static/css/2.9b79e585.chunk.css"
+  },
+  {
+    "revision": "bfcd28b2466f51bbfb2f",
+    "url": "/house-tokens/static/css/main.30b7bd8a.chunk.css"
+  },
+  {
+    "revision": "5222039d7fd86f0da4c9",
+    "url": "/house-tokens/static/js/2.4f63b5b9.chunk.js"
+  },
+  {
+    "revision": "bfcd28b2466f51bbfb2f",
+    "url": "/house-tokens/static/js/main.441f31ea.chunk.js"
+  },
+  {
+    "revision": "f02b2ddf1235f75c2405",
+    "url": "/house-tokens/static/js/runtime-main.6f5c93b1.js"
+  },
+  {
+    "revision": "78ef24a24e913217c75179cd02260e5e",
+    "url": "/house-tokens/static/media/ETH.78ef24a2.png"
+  },
+  {
+    "revision": "c7be19d86c3e1b511822dcadcfb8052f",
+    "url": "/house-tokens/static/media/eth-diamond-rainbow.c7be19d8.png"
+  },
+  {
+    "revision": "50df7b69442e6be96dbf5a07a28aabaa",
+    "url": "/house-tokens/static/media/smiley.50df7b69.jpg"
+  }
+]);
