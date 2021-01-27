17404db8dcdfb720c67bf7c37a78c1c85bce8c98
jlelito <joshualelito@gmail.com>
2021-01-26 23:05:47 -0500

    Removing comments

--- truffle-config.js
+++ truffle-config.js
@@ -3,8 +3,7 @@ require('dotenv').config();
 const HDWalletProvider = require('truffle-hdwallet-provider');
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     develop: {
