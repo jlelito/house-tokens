c6c77d54406d6708c7b9173c337c503fce52b669
jlelito <joshualelito@gmail.com>
2021-01-27 20:09:36 -0500

    Removing MintHouse method from component

--- client/src/MintHouse.js
+++ client/src/MintHouse.js
@@ -1,19 +1,6 @@
 import React, { Component } from 'react';
 
 class MintHouse extends Component {
-
-    mintHouse = (houseAddress, squareFeet, price, bedrooms, bathrooms) => {
-      
-        let ethPrice = window.web3.utils.toWei(price, 'Ether')
-        try{
-        this.props.houseToken.methods.mint(houseAddress, squareFeet, ethPrice, bedrooms, bathrooms).send({ from: this.props.account }).on('transactionHash', (hash) => {
-          window.location.reload();
-          
-        })
-      } catch(e) {
-        window.alert(e)
-      }
-      }
   
     render() {
   
@@ -25,21 +12,20 @@ class MintHouse extends Component {
             <h3 className="my-4">Mint House Token</h3>
             
             <form className="mb-3" 
-            
-                          onSubmit={(event) => {
-                          event.preventDefault()
-                          let homeAddress, homeSquarefeet, homePrice, homeBedrooms, homeBathrooms
-                          homeAddress = this.address.value
-                          homeSquarefeet = this.squareFeet.value.toString()
-                          homePrice = this.price.value.toString()
-                          homeBedrooms = this.bedrooms.value.toString()
-                          homeBathrooms = this.bathrooms.value.toString()
-                          this.mintHouse(homeAddress, homeSquarefeet, homePrice, homeBedrooms, homeBathrooms)
-                          this.address.value = null
-                          this.squareFeet.value = null
-                          this.price.value = null
-                          this.bedrooms.value = null
-                          this.bathrooms.value = null        
+                      onSubmit={(event) => {
+                      event.preventDefault()
+                      let homeAddress, homeSquarefeet, homePrice, homeBedrooms, homeBathrooms
+                      homeAddress = this.address.value
+                      homeSquarefeet = this.squareFeet.value.toString()
+                      homePrice = this.price.value.toString()
+                      homeBedrooms = this.bedrooms.value.toString()
+                      homeBathrooms = this.bathrooms.value.toString()
+                      this.props.mintHouse(homeAddress, homeSquarefeet, homePrice, homeBedrooms, homeBathrooms)
+                      this.address.value = null
+                      this.squareFeet.value = null
+                      this.price.value = null
+                      this.bedrooms.value = null
+                      this.bathrooms.value = null        
               }}>
                       
                       <div className="input-group row justify-content-center">
@@ -65,52 +51,41 @@ class MintHouse extends Component {
                             required /> 
                         </div>
                         
-
-                        
-                          <div className="form-group col-sm-3">
-                            <label className="text-center">Price</label>
-                              <input
-                              type="number"
-                              step=".01"
-                              ref={(price) => { this.price = price }}
-                              className="form-control form-control-lg"
-                              placeholder="0 ETH"
-                              required /> 
-                          </div>
-                        
+                        <div className="form-group col-sm-3">
+                          <label className="text-center">Price</label>
+                            <input
+                            type="number"
+                            step=".01"
+                            ref={(price) => { this.price = price }}
+                            className="form-control form-control-lg"
+                            placeholder="0 ETH"
+                            required /> 
+                        </div>
                       </div>
                         
 
-                        <div className="mb-4 row justify-content-center">
-                          <div className="col-sm-3">
-                            <label className="text-center">Number of Bedrooms</label>
-                              <input
-                              type="number"
-                              ref={(bedrooms) => { this.bedrooms = bedrooms }}
-                              className="form-control form-control-lg"
-                              placeholder="0"
-                              required /> 
-                          </div>
-                          
-
-                          
-                            <div className="col-sm-3">
-                              <label className="text-center">Number of Bathrooms</label>
-                                <input
-                                type="number"
-                                ref={(bathrooms) => { this.bathrooms = bathrooms }}
-                                className="form-control form-control-lg"
-                                placeholder="0"
-                                required /> 
-                            </div>
-                          
+                      <div className="mb-4 row justify-content-center">
+                        <div className="col-sm-3">
+                          <label className="text-center">Number of Bedrooms</label>
+                            <input
+                            type="number"
+                            ref={(bedrooms) => { this.bedrooms = bedrooms }}
+                            className="form-control form-control-lg"
+                            placeholder="0"
+                            required /> 
                         </div>
-                      
-                      
-                          
-                      
-                      <button type="submit" 
-                      className="btn btn-primary  btn-lg">Mint House!</button>
+                        
+                        <div className="col-sm-3">
+                          <label className="text-center">Number of Bathrooms</label>
+                            <input
+                            type="number"
+                            ref={(bathrooms) => { this.bathrooms = bathrooms }}
+                            className="form-control form-control-lg"
+                            placeholder="0"
+                            required /> 
+                        </div>
+                      </div>
+                      <button type="submit" className="btn btn-primary  btn-lg">Mint House!</button>
             </form>
             
             </>
