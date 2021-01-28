2b304f5ce43d2134141304f992f0fa6cb9bbabe9
jlelito <joshualelito@gmail.com>
2021-01-27 21:37:59 -0500

    Removing initial house creating in deploy contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,8 +2,4 @@ var House = artifacts.require("./House.sol");
 
 module.exports = async function(deployer) {
     await deployer.deploy(House);
-    const HouseInstance = await House.deployed();
-    await HouseInstance.mint("Mansion", 1500, 900000000000000, 7, 6);
-    await HouseInstance.mint("ECU", 300, 300000000000000, 1, 1);
-    await HouseInstance.mint("Huge House!", 150000, 500000000000000, 9, 10);
 };
