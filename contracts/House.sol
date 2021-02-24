d7723ede903ff295dcfb6354fc24a70648be3e54
jlelito <joshualelito@gmail.com>
2021-02-23 21:29:02 -0500

    Adding royalty to house sales

--- contracts/House.sol
+++ contracts/House.sol
@@ -7,17 +7,18 @@ contract House is ERC721Full {
   struct House {
     uint houseID;
     address payable owner;
+    address payable royaltyCollector;
     string homeAddress;
     uint sqFeet;
     uint256 price;
     uint bedrooms;
     uint bathrooms;
+    uint royalty;
   }
 
-  event boughtHouse(uint houseID, address owner, string homeAddress, uint sqFeet, uint256 price, uint bedrooms,uint bathrooms);
-  event sentHouse(uint id, address to);
+  event boughtHouse(uint houseID, address seller, address buyer, string homeAddress, uint sqFeet, uint256 price, uint bedrooms, uint bathrooms);
+  event sentHouse(uint id, address from, address to);
   event changedPrice(uint id, uint oldPrice, uint newPrice);
-
 
   mapping(uint => House) public houses;
   mapping(uint256 => bool) _houseExists;
@@ -28,10 +29,10 @@ contract House is ERC721Full {
       admin = msg.sender;
   }
 
-  function mint(string memory _homeAddress, uint _sqFeet, uint256 _price, uint _bedrooms, uint _bathrooms) public {
+  function mint(string memory _homeAddress, uint _sqFeet, uint256 _price, uint _royalty, uint _bedrooms, uint _bathrooms) public {
     require(!_houseExists[nextId], 'this house id already exists');
     require(msg.sender == admin, 'Must be Admin to mint House Tokens');
-    houses[nextId] = House(nextId, msg.sender, _homeAddress, _sqFeet, _price, _bedrooms, _bathrooms);
+    houses[nextId] = House(nextId, msg.sender, msg.sender, _homeAddress, _sqFeet, _price, _royalty, _bedrooms, _bathrooms);
     _mint(msg.sender, nextId);
     _houseExists[nextId] = true;
     nextId+=1;
@@ -41,8 +42,8 @@ contract House is ERC721Full {
     require(ownerOf(houseID) == msg.sender, 'you must own the house');
     require(_houseExists[houseID], 'house does not exist!!');
     _transferFrom(msg.sender, to, houseID);
-    houses[houseID].owner = to;
-    emit sentHouse(houseID, to);
+    address owner = ownerOf(houseID);
+    emit sentHouse(houseID, owner, to);
   }
 
   function changePrice(uint _id, uint newPrice) public {
@@ -60,19 +61,21 @@ contract House is ERC721Full {
     require(_houseExists[_id], 'house does not exist!!');
     //Fetch the House
     House memory _house = houses[_id];
-    require(_house.owner != msg.sender, 'cannot buy your own house!');
+    require(ownerOf(_id) != msg.sender, 'cannot buy your own house!');
     //Make sure send right amount 
     require(msg.value == _house.price, 'not enough ether!');
-    //Fetch the Owner
-    address payable _owner = _house.owner;
-    //Pay the author by sending them Ether
-    address(_owner).transfer(msg.value);
+    //Fetch the House Owner
+    address payable _seller = _house.owner;
+    //Fetch the Royalty Collector
+    address payable _collector = _house.royaltyCollector;
+    //Pay the seller by sending them Ether
+    address(_seller).transfer(msg.value*(1-_house.royalty));
+    //Pay the royalty collector
+    address(_collector).transfer(msg.value*_house.royalty);
     //Send the house token to new owner
-    _transferFrom(_owner, msg.sender, _id);
-    //Change ownership of house
-    houses[_id].owner = msg.sender;
+    _transferFrom(_seller, msg.sender, _id);
     //Emit the image tipped event
-    emit boughtHouse(_id, _house.owner, _house.homeAddress, _house.sqFeet, _house.price, _house.bedrooms, _house.bathrooms);
+    emit boughtHouse(_id, _seller, msg.sender, _house.homeAddress, _house.sqFeet, _house.price, _house.bedrooms, _house.bathrooms);
 
   }
 
